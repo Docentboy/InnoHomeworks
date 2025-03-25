2f5c04bca9ba3fbccd26c4f488e94ddce8e1ab96
Rustam <rustam.khadeyev@gmail.com>
2025-03-25 05:46:50 +0500

    Работа с классами и объектами. IO

---
+++ src/main/java/ru/innopolis/homeworks/homework08/product/DiscountProduct.java
@@ -0,0 +1,60 @@
+package ru.innopolis.homeworks.homework08.product;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class DiscountProduct extends Product {
+
+    private Integer discount;
+    private LocalDate discountDate;
+
+    public DiscountProduct(String name, int price, Integer discount, LocalDate discountDate) {
+        super(name, price);
+        this.setDiscount(discount);
+        this.setDiscountDate(discountDate);
+
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Integer discount) {
+        if (discount == null || discount < 0) {
+            throw new IllegalArgumentException("Скидка не должна быть отрицательной");
+        }
+        this.discount = discount;
+    }
+
+    public LocalDate getDiscountDate() {
+        return discountDate;
+    }
+
+    public void setDiscountDate(LocalDate discountDate) {
+        this.discountDate = discountDate;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        DiscountProduct that = (DiscountProduct) o;
+        return Objects.equals(discount, that.discount) && Objects.equals(discountDate, that.discountDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), discount, discountDate);
+    }
+
+    @Override
+    public String toString() {
+        return "DiscountProduct{" +
+                "name=" + super.getName() +
+                ", price=" + super.getPrice() +
+                ", discount=" + discount +
+                ", discountDate=" + discountDate +
+                '}';
+    }
+}
---
+++ src/main/java/ru/innopolis/homeworks/homework08/product/Product.java
@@ -0,0 +1,59 @@
+package ru.innopolis.homeworks.homework08.product;
+
+import ru.innopolis.homeworks.homework06.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.setName(name);
+        this.setPrice(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty() || name.matches("\\d+") || name.length() < 3) {
+            throw new IllegalArgumentException("Недопустимое имя продукта!");
+        }
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException("Недопустимая стоимость продукта!");
+        }
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
